e2579cf9333f7d9220eb70d1b1b664ac48581c3a
lanly_dev <lanly-dev@users.noreply.github.com>
2021-10-31 16:15:05 -0700

    import-checking

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from '@vuetify/vite-plugin'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,24 @@ export default defineConfig({
       autoImport: true,
       styles: true
     })
-  ]
+  //   {
+  //     name: 'import-checking',
+  //     transform(src, id) {
+  //       if (/.vue/.test(id)) {
+  //         console.log('\n\n')
+  //         console.log('$$$$', id)
+  //         for (const i of src.split('\n')) {
+  //           if (i.startsWith('import')) console.log('--->', i)
+  //         }
+  //         console.log('\n\n')
+  //         return { code: src }
+  //       }
+  //     }
+  //   }
+  // ],
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src')
+  //   }
+  // }
 })
